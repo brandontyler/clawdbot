f2c8f8303101f5c8a58027b9f8fec45f47c4015a
tylerbtt <tylerbtt@amazon.com>
2026-02-20 16:13:21 -0600

    discord: log successful resume/reconnect after WebSocket disconnect

--- extensions/discord/src/gateway-logging.ts
+++ extensions/discord/src/gateway-logging.ts
@@ -8,6 +8,7 @@ const INFO_DEBUG_MARKERS = [
   "WebSocket connection closed",
   "Reconnecting with backoff",
   "Attempting resume with backoff",
+  "Resumed successfully",
 ];
 
 const shouldPromoteGatewayDebug = (message: string) =>
